c35f9a2faf3cdbb5327092b9c3109ba0bb4412f9
m <you@example.com>
2018-11-08 14:59:13 -0500

    Fix path images

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
+    publicPath: 'dist/',
     // publicPath: path.resolve(__dirname, 'dist') + '/',
     filename: 'build.js'
   },
